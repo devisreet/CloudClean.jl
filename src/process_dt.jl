9cc7e88efa70339083c9d3b6a4b1af632d98362e
Devisree Tallapaneni <devisreet@gmail.com>
2023-06-13 00:10:25 -0400

    Print statements

--- src/process_dt.jl
+++ src/process_dt.jl
@@ -611,7 +611,7 @@ function varyr_chi_squared_stats(x_locs,y_locs,raw_image,img;Np=33, widx=129,wid
         circmask = .!kstar_circle_mask(Np,rlim=r^2);
         bimage[(cenx-dv):(cenx+dv),(ceny-dv):(ceny+dv)].=circmask;
         raw_image[bimage].=0;
-        print(count(bimage))
+        count(bimage)
     
         chi_squared_vals = chi_squared_stats(x_locs,y_locs,raw_image,bimage,img,Np=Np,widx=widx,widy=widy,tilex=tilex,ftype=ftype,tiley=tiley,seed=seed,rlim=rlim,ndraw=ndraw, infill_num=infill_num)
         
